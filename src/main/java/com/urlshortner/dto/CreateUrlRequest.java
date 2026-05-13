b15a1c1a3142a4295512da73d6c80dd8bd7d16d9
ahvsasankbabu <ahvsasankbabu@gmail.com>
2026-05-13 19:29:47 +0530

    Added validation for empty URL requests

--- src/main/java/com/urlshortner/dto/CreateUrlRequest.java
+++ src/main/java/com/urlshortner/dto/CreateUrlRequest.java
@@ -5,7 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 public class CreateUrlRequest {
 	@NotBlank(message = "URL cannot be empty")
 	private String longUrl;
-
+	
+	@NotBlank(message= "\"URL cannot be empty")
 	public String getLongUrl() {
 		return longUrl;
 	}
